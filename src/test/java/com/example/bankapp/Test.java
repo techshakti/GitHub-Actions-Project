8e7a28834cd34133ccaf282d8d91f55d97489403
Shakti Samal <shaktisamal@gmail.com>
2026-03-26 08:04:25 +0530

    updated Test class

--- src/test/java/com/example/bankapp/Test.java
+++ src/test/java/com/example/bankapp/Test.java
@@ -1,21 +1,10 @@
 package com.example.bankapp;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class BankappApplicationTests {
 
-    @Test
-    void sampleTest() {
-        int expected = 42;
-        int actual = 40 + 2;
-        assertEquals(expected, actual, "Simple addition should equal 42");
+class Tests {
+    public static void main(String[] args){
+        System.out.println("Hi Test!!!!");
     }
 
-    @Test
-    void anotherSampleTest() {
-        String message = "Hello, BankApp!";
-        assertNotNull(message, "Message should not be null");
-        assertTrue(message.contains("BankApp"), "Message should contain 'BankApp'");
-    }
 }
